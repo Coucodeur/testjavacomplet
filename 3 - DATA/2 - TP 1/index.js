19329728600245fc7e1c60d2ad38cd8b9aac1215
Coucodeur <matcou68@gmail.com>
2023-06-04 14:42:11 +0200

    rajout du premier tp

---
+++ 3 - DATA/2 - TP 1/index.js
@@ -0,0 +1,10 @@
+let red = 0;
+let gre = 0;
+let blu = 0;
+let rgb = 0;
+body = document.querySelector("body");
+console.log(body);
+const colorchange = () => {
+  body.style.background = (0, 0, 0);
+};
+colorchange;
