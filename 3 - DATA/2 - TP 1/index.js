67d488f5915c567961eeac7e8fc49f396fadb4b9
Coucodeur <matcou68@gmail.com>
2023-06-04 21:33:06 +0200

    On a fait le TP1(body change color)... maintenant on reprend le cours complet avant de partir sur le PT2

--- 3 - DATA/2 - TP 1/index.js
+++ 3 - DATA/2 - TP 1/index.js
@@ -1,10 +1,16 @@
-let red = 0;
-let gre = 0;
-let blu = 0;
-let rgb = 0;
-body = document.querySelector("body");
-console.log(body);
-const colorchange = () => {
-  body.style.background = (0, 0, 0);
+// const body = document.querySelector("body");
+// let red = 0;
+// let gre = 0;
+// let blu = 0;
+// let rgb = 0;
+const rgbIs = document.querySelector(".rgbIs");
+
+const colorChange = () => {
+  let red = Math.floor(Math.random() * 255);
+  let gre = Math.floor(Math.random() * 255);
+  let blu = Math.floor(Math.random() * 255);
+  let rgb = `rgb(${red}, ${gre}, ${blu})`;
+  document.body.style.backgroundColor = rgb;
+  rgbIs.textContent = rgb;
 };
-colorchange;
+setInterval(colorChange, 2000);
